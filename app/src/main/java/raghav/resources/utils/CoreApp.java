1705a30029f3654de44224c0e7b32072a2b06026
Raghav Satyadev <raghav.satyadev@drcsystems.com>
2016-11-08 13:47:42 +0530

    added AppForegroundChecker

--- app/src/main/java/raghav/resources/utils/CoreApp.java
+++ app/src/main/java/raghav/resources/utils/CoreApp.java
@@ -1,6 +1,7 @@
 package raghav.resources.utils;
 
 import android.app.Application;
+import android.widget.Toast;
 
 /**
  * Created by raghav on 4/11/16.
@@ -9,13 +10,30 @@ import android.app.Application;
 public class CoreApp extends Application {
     private static CoreApp mInstance;
 
+    AppForegroundChecker.Listener listener = new AppForegroundChecker.Listener() {
+        public void onBecameForeground() {
+            Toast.makeText(CoreApp.this, "foreground", Toast.LENGTH_SHORT).show();
+        }
+
+        public void onBecameBackground() {
+            Toast.makeText(CoreApp.this, "background", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public static synchronized CoreApp getInstance() {
+        return mInstance;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
         mInstance = this;
+        AppForegroundChecker.get(mInstance).addListener(listener);
     }
 
-    public static synchronized CoreApp getInstance() {
-        return mInstance;
+    @Override
+    public void onTerminate() {
+        AppForegroundChecker.get(getInstance()).removeListener(listener);
+        super.onTerminate();
     }
 }
