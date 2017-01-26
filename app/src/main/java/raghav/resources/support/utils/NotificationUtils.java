c05d838731272c5dce59de39583c60f2e70fa1ad
Raghav Satyadev <raghav.satyadev@drcsystems.com>
2017-01-26 12:31:45 +0530

    - updated gitignore - notification util updated

--- app/src/main/java/raghav/resources/support/utils/NotificationUtils.java
+++ app/src/main/java/raghav/resources/support/utils/NotificationUtils.java
@@ -1,5 +1,13 @@
 package raghav.resources.support.utils;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+
 import raghav.resources.R;
 
 /**
@@ -11,5 +19,21 @@ public class NotificationUtils {
     public static int getSmallIcon() {
         boolean useWhiteIcon = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
         return useWhiteIcon ? /*R.drawable.ic_small_icon*/ 0 : R.mipmap.ic_launcher;
+    }
+
+    public static NotificationCompat.Builder buildNotification(Context context, String message, PendingIntent pendingIntent) {
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        return new NotificationCompat.Builder(context)
+                .setContentTitle(ResourceUtils.getString(R.string.app_name))
+                .setAutoCancel(true)
+                .setSmallIcon(getSmallIcon())
+                .setLargeIcon(((BitmapDrawable) ContextCompat.getDrawable(context, R.mipmap.ic_launcher)).getBitmap())
+                .setSound(defaultSoundUri)
+                .setContentText(message)
+                .setContentIntent(pendingIntent)
+                .setColor(ResourceUtils.getColor(R.color.icon_background))
+                .setTicker(message)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(message));
     }
 }
