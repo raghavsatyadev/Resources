bdf93f3dfc3997acd4e83d0cbc742d67297f8538
Raghav Satyadev <raghav.satyadev@drcsystems.com>
2017-02-21 16:07:15 +0530

    - added password validation method in validators

--- app/src/main/java/raghav/resources/support/utils/Validators.java
+++ app/src/main/java/raghav/resources/support/utils/Validators.java
@@ -3,6 +3,8 @@ package raghav.resources.support.utils;
 import android.text.TextUtils;
 import android.util.Patterns;
 
+import java.util.regex.Pattern;
+
 
 public class Validators {
     public static boolean isValidEmail(String email) {
@@ -11,5 +13,23 @@ public class Validators {
 
     public static boolean isValidMobile(String phone) {
         return phone.length() > 9 && Patterns.PHONE.matcher(phone).matches();
+    }
+
+    /**
+     * Checks for 1 Uppercase Alphabet, 1 Number, 1 Special Character and at least 8 character length
+     *
+     * @param password
+     * @return
+     */
+    public static boolean isValidPassword(String password) {
+        password = password.trim();
+        if (!TextUtils.isEmpty(password)) {
+            String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+
+            return Pattern.compile(PASSWORD_PATTERN).matcher(password.trim()).matches() && password.length() >= 8;
+        } else {
+            return false;
+        }
+
     }
 }
