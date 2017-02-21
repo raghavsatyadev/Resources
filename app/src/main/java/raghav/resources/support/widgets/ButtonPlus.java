e471c753f2ecd1d3d525c333e3944a0056724726
Raghav Satyadev <raghav.satyadev@drcsystems.com>
2017-02-21 11:49:26 +0530

    - widgets updated

--- app/src/main/java/raghav/resources/support/widgets/ButtonPlus.java
+++ app/src/main/java/raghav/resources/support/widgets/ButtonPlus.java
@@ -1,17 +1,11 @@
 package raghav.resources.support.widgets;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Typeface;
 import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
 
-import raghav.resources.R;
-import raghav.resources.support.utils.ResourceUtils;
-
 public class ButtonPlus extends AppCompatButton {
-    private static Typeface typeface = null;
-    private static String customFont = "";
+
 
     public ButtonPlus(Context context) {
         super(context);
@@ -19,31 +13,13 @@ public class ButtonPlus extends AppCompatButton {
 
     public ButtonPlus(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setCustomTypeFace(context, attrs);
+        CustomFontHelper.setCustomTypeFace(this, context, attrs);
     }
 
     public ButtonPlus(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setCustomTypeFace(context, attrs);
+        CustomFontHelper.setCustomTypeFace(this, context, attrs);
     }
 
-    private void setCustomTypeFace(Context context, AttributeSet set) {
-        if (set != null) {
-            TypedArray a = context.obtainStyledAttributes(set, R.styleable.WidgetPlus);
-            String fontLink = a.getString(R.styleable.WidgetPlus_textFont);
-            a.recycle();
-            if (fontLink == null) {
-                fontLink = ResourceUtils.getString(R.string.default_font);
-            }
-            if (!customFont.equals(fontLink)) {
-                customFont = fontLink;
-                try {
-                    typeface = Typeface.createFromAsset(context.getAssets(), "font/" + customFont);
-                } catch (Exception e) {
-                    return;
-                }
-            }
-            setTypeface(typeface);
-        }
-    }
+
 }
--- app/src/main/java/raghav/resources/support/widgets/EditTextPlus.java
+++ app/src/main/java/raghav/resources/support/widgets/EditTextPlus.java
@@ -1,18 +1,10 @@
 package raghav.resources.support.widgets;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Typeface;
 import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
 
-import raghav.resources.R;
-import raghav.resources.support.utils.ResourceUtils;
-
-
 public class EditTextPlus extends AppCompatEditText {
-    private static Typeface typeface = null;
-    private static String customFont = "";
 
     public EditTextPlus(Context context) {
         super(context);
@@ -20,31 +12,11 @@ public class EditTextPlus extends AppCompatEditText {
 
     public EditTextPlus(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setCustomTypeFace(context, attrs);
+        CustomFontHelper.setCustomTypeFace(this, context, attrs);
     }
 
     public EditTextPlus(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setCustomTypeFace(context, attrs);
-    }
-
-    private void setCustomTypeFace(Context context, AttributeSet set) {
-        if (set != null) {
-            TypedArray a = context.obtainStyledAttributes(set, R.styleable.WidgetPlus);
-            String fontLink = a.getString(R.styleable.WidgetPlus_textFont);
-            a.recycle();
-            if (fontLink == null) {
-                fontLink = ResourceUtils.getString(R.string.default_font);
-            }
-            if (!customFont.equals(fontLink)) {
-                customFont = fontLink;
-                try {
-                    typeface = Typeface.createFromAsset(context.getAssets(), "font/" + customFont);
-                } catch (Exception e) {
-                    return;
-                }
-            }
-            setTypeface(typeface);
-        }
+        CustomFontHelper.setCustomTypeFace(this, context, attrs);
     }
 }
