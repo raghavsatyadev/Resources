b67854ba31557ae474a4de8f1e808a38589591a3
Raghav Satyadev <raghav.satyadev@drcsystems.com>
2017-03-17 17:31:42 +0530

    - added retrofit - code optimised - bugs resolved - added JAVA 8 compatibility - proguard updated

--- app/src/main/java/raghav/resources/SplashActivity.java
+++ app/src/main/java/raghav/resources/SplashActivity.java
@@ -8,6 +8,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+//        setContentView(R.layout.activity_splash);
     }
 }
