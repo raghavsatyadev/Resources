f1d4033805b3b539887c047b731b8597ddd25154
Raghav Satyadev <raghav.satyadev@drcsystems.com>
2017-03-30 13:46:04 +0530

    - gradle upgrade

--- app/src/main/java/raghav/resources/SplashActivity.java
+++ app/src/main/java/raghav/resources/SplashActivity.java
@@ -1,13 +1,24 @@
 package raghav.resources;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+import raghav.resources.support.base.CoreActivity;
+
+public class SplashActivity extends CoreActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_splash);
+        setDefaults(R.layout.activity_splash);
+    }
+
+    @Override
+    public void createReference() {
+
+    }
+
+    @Override
+    protected void setListeners(boolean state) {
+
     }
 }
