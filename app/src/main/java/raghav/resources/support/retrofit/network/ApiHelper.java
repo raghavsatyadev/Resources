82969de4b3c01e2a108f92eeed6958329f843862
raghav satyadev <raghavsatyadev@gmail.com>
2017-06-24 14:45:22 +0530

    change in applog.java

--- app/src/main/java/raghav/resources/support/retrofit/network/ApiHelper.java
+++ app/src/main/java/raghav/resources/support/retrofit/network/ApiHelper.java
@@ -1,12 +1,5 @@
 package raghav.resources.support.retrofit.network;
 
-import android.text.TextUtils;
-
-import java.io.File;
-
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
 import raghav.resources.support.utils.AppLog;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,32 +19,6 @@ public class ApiHelper<T> {
         }
 
         return apiHelper;
-    }
-
-    public static MultipartBody.Part getMultiPartImage(String fileParameter, File file) {
-        if (file != null)
-            return MultipartBody.Part.createFormData(fileParameter, file.getName(),
-                    RequestBody.create(MediaType.parse("image/*"), file));
-        return null;
-    }
-
-    public static MultipartBody.Part getMultiPartImage(String fileParameter, String filepath) {
-        if (!TextUtils.isEmpty(filepath)) {
-            File file = new File(filepath);
-            try {
-                return getMultiPartImage(fileParameter, file);
-            } catch (Exception e) {
-                AppLog.log(false, "ApiHelper " + "getMultiPartImage: ", e);
-                return null;
-            }
-        } else {
-            return null;
-        }
-
-    }
-
-    public static RequestBody getMultipartString(String string) {
-        return RequestBody.create(MediaType.parse("text/plain"), string);
     }
 
     public void callApi(Call<T> call, final APIResponse retrofitApiResponse, final ApiNames apiNames) {
