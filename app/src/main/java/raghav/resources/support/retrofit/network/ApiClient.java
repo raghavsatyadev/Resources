3ed557129bfcd2f8030853c5606a8673b8260464
raghav <raghavsatyadev@gmail.com>
2017-07-10 16:20:03 +0530

    - applog updated

--- app/src/main/java/raghav/resources/support/retrofit/network/ApiClient.java
+++ app/src/main/java/raghav/resources/support/retrofit/network/ApiClient.java
@@ -64,30 +64,39 @@ public class ApiClient {
                 builder.addInterceptor(loggingInterceptor);
             }
 
-//            Code for basic Authentication
-            if (!TextUtils.isEmpty(WebService.API_USERNAME) && !TextUtils.isEmpty(WebService.API_PASSWORD)) {
-                String credentials = WebService.API_USERNAME + ":" + WebService.API_PASSWORD;
-                final String basic =
-                        "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+//            addBasicAuth(builder);
 
-                builder.addInterceptor(new Interceptor() {
-                    @Override
-                    public Response intercept(Chain chain) throws IOException {
-                        Request original = chain.request();
-
-                        Request.Builder requestBuilder = original.newBuilder()
-                                .header("Authorization", basic)
-                                .header("Accept", "application/json")
-                                .method(original.method(), original.body());
-
-                        Request request = requestBuilder.build();
-                        return chain.proceed(request);
-                    }
-                });
-            }
             okHttpClient = builder.build();
         }
         return okHttpClient;
+    }
+
+    /**
+     * Code for basic Authentication
+     *
+     * @param builder OkHttpClient.Builder
+     */
+    private static void addBasicAuth(OkHttpClient.Builder builder) {
+        if (!TextUtils.isEmpty(WebService.API_USERNAME) && !TextUtils.isEmpty(WebService.API_PASSWORD)) {
+            String credentials = WebService.API_USERNAME + ":" + WebService.API_PASSWORD;
+            final String basic =
+                    "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+
+            builder.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request original = chain.request();
+
+                    Request.Builder requestBuilder = original.newBuilder()
+                            .header("Authorization", basic)
+                            .header("Accept", "application/json")
+                            .method(original.method(), original.body());
+
+                    Request request = requestBuilder.build();
+                    return chain.proceed(request);
+                }
+            });
+        }
     }
 
     public interface WebService {
