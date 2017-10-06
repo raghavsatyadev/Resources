9a6a3d14869ae784254248e803031901a19dde2f
Raghav Satyadev <raghavsatyadev@gmail.com>
2017-10-06 14:50:17 +0530

    - ImageChooser Util Updated

--- support/src/main/java/com/support/utils/ImageChooserUtil.java
+++ support/src/main/java/com/support/utils/ImageChooserUtil.java
@@ -1,6 +1,5 @@
 package com.support.utils;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ContentResolver;
@@ -13,11 +12,9 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
-import android.text.TextUtils;
 
 import com.support.base.CoreApp;
 
@@ -26,62 +23,98 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
-import static android.app.Activity.RESULT_OK;
+/* Usage:
 
+     1. declare provider in manifest
 
-public class MultiImageChooserUtil {
+     <provider
+        android:name="android.support.v4.content.FileProvider"
+        android:authorities="${applicationId}.fileprovider"
+        android:exported="false"
+        android:grantUriPermissions="true">
+        <meta-data
+        android:name="android.support.FILE_PROVIDER_PATHS"
+        android:resource="@xml/image_path" />
+        </provider>
 
-    public final int REQUEST_GALLERY;
-    public final int REQUEST_CAMERA;
-    public final int PERMISSION_CAMERA;
-    public final int PERMISSION_WRITE_STORAGE;
-    private final String IMAGE_DIRECTORY = "Images";
-    private final String CAPTURE_IMAGE_FILE_PROVIDER = ".fileprovider";
-    private String FILE_EXTENSION = ".jpg";
-    private String fileName;
+     2. write permission and feature in manifest
 
-    private MultiImageChooserUtil() {
-        REQUEST_GALLERY = 1233;
-        REQUEST_CAMERA = 1234;
-        PERMISSION_CAMERA = 1236;
-        PERMISSION_WRITE_STORAGE = 1237;
-    }
+     <uses-permission android:name="android.permission.CAMERA" />
 
-    private MultiImageChooserUtil(int requestCode) {
-        if (requestCode != 0) {
-            REQUEST_CAMERA = requestCode + 1;
-            REQUEST_GALLERY = requestCode;
-            PERMISSION_CAMERA = requestCode + 1;
-            PERMISSION_WRITE_STORAGE = requestCode;
-        } else {
-            REQUEST_GALLERY = 1233;
-            REQUEST_CAMERA = 1234;
-            PERMISSION_CAMERA = 1236;
-            PERMISSION_WRITE_STORAGE = 1237;
+     <uses-feature
+        android:name="android.hardware.camera"
+        android:required="false" />
+
+     3. xml file for provider resource
+
+     <paths>
+        <files-path name="captured_image" path="Images/" />
+     </paths>
+
+     4. calling code
+
+        ImageChooserUtil.openChooserDialog(coreFragment,"fileName");
+
+     4. add in requesting activity
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            switch (requestCode) {
+                case ImageChooserUtil.PERMISSION_WRITE_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager
+                        .PERMISSION_GRANTED) {
+                    ImageChooserUtil.openChooserDialog(getCoreFragment(), getViewModel().cvPhotoName);
+                }
+                break;
+            case ImageChooserUtil.REQUEST_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager
+                        .PERMISSION_GRANTED) {
+                    ImageChooserUtil.startCameraIntent(getCoreFragment(), getViewModel().cvPhotoName);
+                }
+                break;
+            }
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-    }
 
-    public static MultiImageChooserUtil getInstance(int requestCode) {
-        return new MultiImageChooserUtil(requestCode);
-    }
+      5. Add onActivityResult
 
-    public static MultiImageChooserUtil getInstance() {
-        return new MultiImageChooserUtil();
-    }
+       @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            switch (requestCode) {
+                case ImageChooserUtil.REQUEST_GALLERY:
+                case ImageChooserUtil.REQUEST_CAMERA:
+                    if (resultCode == RESULT_OK) {
+                        new ImageChooserUtil.SaveImageTask(activity,
+                                data,
+                                requestCode,
+                                String.valueOf(goodsFileName),
+                                new ImageChooserUtil.SaveImageTask.FileSaveListener() {
+                                    @Override
+                                    public void fileSaved(File file) {
 
-    public void openChooserDialog(final Activity activity) {
-        if (!TextUtils.isEmpty(fileName)) {
-            openChooserDialog(activity, fileName);
-        } else
-            throw new RuntimeException("you have to call openChooserDialog(final Activity activity, final String fileName) first");
-    }
+                                    }
+                                }).execute();
+                    }
+                    break;
+            }
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+      */
+public class ImageChooserUtil {
+
+    public static final int REQUEST_GALLERY = 1235;
+    public static final int REQUEST_CAMERA = 1234;
+    public static final int PERMISSION_CAMERA = 1236;
+    public static final int PERMISSION_WRITE_STORAGE = 1237;
+    private static final String IMAGE_DIRECTORY = "Images";
+    private static final String CAPTURE_IMAGE_FILE_PROVIDER = ".fileprovider";
+    private static String FILE_EXTENSION = ".jpg";
 
     /**
      * @param fileName keep file name in field. this will be required when getting permission.
      */
-    public void openChooserDialog(final Activity activity, final String fileName) {
-        this.fileName = fileName;
-
+    public static void openChooserDialog(final Activity activity, final String fileName) {
         if (PermissionUtil.checkPermission(activity, PermissionUtil.Permissions.WRITE_EXTERNAL_STORAGE)) {
             AlertDialog.Builder builder = new AlertDialog.Builder(activity);
             builder.setTitle("Choose Image")
@@ -89,7 +122,7 @@ public class MultiImageChooserUtil {
                         @Override
                         public void onClick(DialogInterface dialog, int id) {
                             if (PermissionUtil.checkPermission(activity, PermissionUtil.Permissions.CAMERA)) {
-                                startCameraIntent(activity);
+                                startCameraIntent(activity, fileName);
                             } else {
                                 PermissionUtil.getPermission(activity,
                                         PermissionUtil.Permissions.CAMERA,
@@ -126,19 +159,10 @@ public class MultiImageChooserUtil {
         }
     }
 
-    public void openChooserDialog(final Fragment fragment) {
-        if (!TextUtils.isEmpty(fileName)) {
-            openChooserDialog(fragment, fileName);
-        } else
-            throw new RuntimeException("you have to call openChooserDialog(final Fragment fragment, final String fileName) first");
-    }
-
     /**
      * @param fileName keep file name in field. this will be required when getting permission.
      */
-    public void openChooserDialog(final Fragment fragment, final String fileName) {
-        this.fileName = fileName;
-
+    public static void openChooserDialog(final Fragment fragment, final String fileName) {
         if (PermissionUtil.checkPermission(fragment.getContext(), PermissionUtil.Permissions.WRITE_EXTERNAL_STORAGE)) {
             AlertDialog.Builder builder = new AlertDialog.Builder(fragment.getContext());
 
@@ -147,7 +171,7 @@ public class MultiImageChooserUtil {
                         @Override
                         public void onClick(DialogInterface dialog, int id) {
                             if (PermissionUtil.checkPermission(fragment.getContext(), PermissionUtil.Permissions.CAMERA)) {
-                                startCameraIntent(fragment);
+                                startCameraIntent(fragment, fileName);
                             } else {
                                 PermissionUtil.getPermission(fragment,
                                         PermissionUtil.Permissions.CAMERA,
@@ -184,22 +208,21 @@ public class MultiImageChooserUtil {
         }
     }
 
-    private void startGalleryIntent(Activity activity) {
+    private static void startGalleryIntent(Activity activity) {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         activity.startActivityForResult(Intent.createChooser(intent, "Select File"), REQUEST_GALLERY);
     }
 
-    private void startGalleryIntent(Fragment fragment) {
+    private static void startGalleryIntent(Fragment fragment) {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         fragment.startActivityForResult(Intent.createChooser(intent, "Select File"), REQUEST_GALLERY);
     }
 
-    @SuppressLint("ObsoleteSdkInt")
-    public void startCameraIntent(Activity activity) {
+    public static void startCameraIntent(Activity activity, String fileName) {
         File path = new File(StorageUtils.createInternalDirectory(), IMAGE_DIRECTORY);
 
         if (!path.exists()) path.mkdirs();
@@ -234,8 +257,7 @@ public class MultiImageChooserUtil {
         activity.startActivityForResult(intent, REQUEST_CAMERA);
     }
 
-    @SuppressLint("ObsoleteSdkInt")
-    public void startCameraIntent(Fragment fragment) {
+    public static void startCameraIntent(Fragment fragment, String fileName) {
         File path = new File(StorageUtils.createInternalDirectory(), IMAGE_DIRECTORY);
 
         if (!path.exists()) path.mkdirs();
@@ -270,7 +292,7 @@ public class MultiImageChooserUtil {
         fragment.startActivityForResult(intent, REQUEST_CAMERA);
     }
 
-    private File getGalleryImageFile(Intent data, ContentResolver resolver) {
+    public static File getGalleryImageFile(Intent data, ContentResolver resolver, String fileName) {
         if (data != null) {
             try {
                 return saveImageToStorage(
@@ -278,7 +300,7 @@ public class MultiImageChooserUtil {
                         IMAGE_DIRECTORY,
                         fileName);
             } catch (IOException e) {
-                AppLog.log(false, "MultiImageChooserUtil " + "getGalleryImageFile: ", e);
+                AppLog.log(false, "ImageChooserUtil " + "getGalleryImageFile: ", e);
                 return null;
             }
         } else {
@@ -286,13 +308,13 @@ public class MultiImageChooserUtil {
         }
     }
 
-    private File getCameraImageFile() {
+    public static File getCameraImageFile(String fileName) {
         File path = new File(StorageUtils.createInternalDirectory(), IMAGE_DIRECTORY);
         if (!path.exists()) path.mkdirs();
         return new File(path, fileName + FILE_EXTENSION);
     }
 
-    private File saveImageToStorage(Bitmap finalBitmap, String path, String imageName) {
+    public static File saveImageToStorage(Bitmap finalBitmap, String path, String imageName) {
         String root = StorageUtils.createInternalDirectory();
         File myDir = new File(root + "/" + path);
         if (!myDir.exists()) {
@@ -309,87 +331,39 @@ public class MultiImageChooserUtil {
             out.close();
             return file;
         } catch (Exception e) {
-            AppLog.log(false, "MultiImageChooserUtil " + "saveImageToStorage: ", e);
+            AppLog.log(false, "ImageChooserUtil " + "saveImageToStorage: ", e);
             return file;
         }
     }
 
-    public boolean resolveOnActivityResult(int requestCode, int resultCode, Intent data, FileSaveListener fileSaveListener) {
-        if (requestCode == REQUEST_CAMERA || requestCode == REQUEST_GALLERY) {
-            if (resultCode == RESULT_OK) {
-                saveImage(data, requestCode, fileSaveListener);
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean resolveOnRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, Activity activity) {
-
-        if (PERMISSION_WRITE_STORAGE == requestCode) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager
-                    .PERMISSION_GRANTED) {
-                openChooserDialog(activity);
-            }
-            return true;
-        } else if (REQUEST_CAMERA == requestCode) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager
-                    .PERMISSION_GRANTED) {
-                startCameraIntent(activity);
-            }
-            return true;
-        } else return false;
-    }
-
-    public boolean resolveOnRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, Fragment fragment) {
-
-        if (PERMISSION_WRITE_STORAGE == requestCode) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager
-                    .PERMISSION_GRANTED) {
-                openChooserDialog(fragment);
-            }
-            return true;
-        } else if (REQUEST_CAMERA == requestCode) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager
-                    .PERMISSION_GRANTED) {
-                startCameraIntent(fragment);
-            }
-            return true;
-        } else return false;
-    }
-
-    public void saveImage(Intent data, int requestCode,
-                          FileSaveListener listener) {
-        new SaveImageTask(data, requestCode, listener).execute();
-    }
-
-    public interface FileSaveListener {
-        void fileSaved(File file);
-    }
-
-    public class SaveImageTask extends AsyncTask<Void, Void, File> {
+    public static class SaveImageTask extends AsyncTask<Void, Void, File> {
 
         private Intent data;
         private int requestCode;
+        private String fileName;
         private FileSaveListener listener;
 
         public SaveImageTask(Intent data,
-                             int requestCode, FileSaveListener listener) {
+                             int requestCode,
+                             String fileName,
+                             FileSaveListener listener) {
             this.data = data;
             this.requestCode = requestCode;
+            this.fileName = fileName;
             this.listener = listener;
         }
 
         @Override
         protected File doInBackground(Void... pObjects) {
             File file = null;
-            if (requestCode == REQUEST_GALLERY) {
-                file = getGalleryImageFile(data,
-                        CoreApp.getInstance().getContentResolver()
-                );
-            } else if (requestCode == REQUEST_CAMERA) {
-                file = getCameraImageFile();
+
+            if (requestCode == ImageChooserUtil.REQUEST_GALLERY) {
+                file = ImageChooserUtil
+                        .getGalleryImageFile(data,
+                                CoreApp.getInstance().getContentResolver()
+                                , String.valueOf(fileName));
+            } else if (requestCode == ImageChooserUtil.REQUEST_CAMERA) {
+                file = ImageChooserUtil.getCameraImageFile(String.valueOf(fileName));
             }
 
             return file;
@@ -402,5 +376,8 @@ public class MultiImageChooserUtil {
             super.onPostExecute(file);
         }
 
+        public interface FileSaveListener {
+            void fileSaved(File file);
+        }
     }
 }
