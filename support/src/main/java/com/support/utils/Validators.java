97d8af766713d7c9e659f1c8f4b2c1db18ad800a
Raghav-Omen <raghavsatyadev@gmail.com>
2018-12-01 18:06:27 +0530

    - validator edited

--- support/src/main/java/com/support/utils/Validators.java
+++ support/src/main/java/com/support/utils/Validators.java
@@ -23,7 +23,7 @@ public class Validators {
     public static boolean isValidPassword(String password) {
         password = password.trim();
         if (!TextUtils.isEmpty(password)) {
-            String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+            String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
 
             return Pattern.compile(PASSWORD_PATTERN).matcher(password.trim()).matches() && password.length() >= 8;
         } else {
